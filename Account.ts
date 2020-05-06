1e8ee8a0a4bffa94a0dd83ea393a5df28229fe08
tanocarfi <tanocarfi@gmail.com>
2020-05-06 12:10:48 +0200

    this is a test

--- Account.ts
+++ Account.ts
@@ -1,13 +1,19 @@
 export class Account {
-    constructor (public id: number, public name: string, public surname: string, public credit = 2000) {}
+  constructor(
+    public id: number,
+    public name: string,
+    public surname: string,
+    public credit = 2000
+  ) {}
+  // this is a comment for testing
 
-    sendMoney (howMoney: number, otherAccount: Account): boolean {
-        if( howMoney > this.credit) {
-            return false;
-        } else {
-            this.credit -= howMoney;
-            otherAccount.credit += howMoney;
-            return true;
-        }
+  sendMoney(howMoney: number, otherAccount: Account): boolean {
+    if (howMoney > this.credit) {
+      return false;
+    } else {
+      this.credit -= howMoney;
+      otherAccount.credit += howMoney;
+      return true;
     }
+  }
 }
